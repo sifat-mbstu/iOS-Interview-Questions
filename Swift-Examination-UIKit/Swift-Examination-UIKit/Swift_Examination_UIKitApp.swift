6855692e7b1f00e70d803c072f0b5b49ed2fd652
Sifatul Islam <sifat.tn99456@gmail.com>
2026-06-27 10:12:16 +0600

    Added UIKit Repo

---
+++ Swift-Examination-UIKit/Swift-Examination-UIKit/Swift_Examination_UIKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_Examination_UIKitApp.swift
+//  Swift-Examination-UIKit
+//
+//  Created by Sifatul on 27/6/26.
+//
+
+import SwiftUI
+
+@main
+struct Swift_Examination_UIKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
