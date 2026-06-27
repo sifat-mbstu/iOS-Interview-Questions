71bdf8e01535aea3cd902712dd5010447f608b01
Sifatul Islam <sifat.tn99456@gmail.com>
2026-06-27 10:11:01 +0600

    initial repo

---
+++ Swift-Examination/Swift-Examination/Swift_ExaminationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_ExaminationApp.swift
+//  Swift-Examination
+//
+//  Created by Sifatul on 27/6/26.
+//
+
+import SwiftUI
+
+@main
+struct Swift_ExaminationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
