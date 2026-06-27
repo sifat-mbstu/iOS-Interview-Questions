6e5b3bf04fcbb742e2d2c84783d4e0d66ac465a9
Sifatul Islam <sifat.tn99456@gmail.com>
2026-06-27 11:14:00 +0600

    added new repos based on uikit and swiftui

---
+++ SwiftUI-Examination/SwiftUI-Examination/SwiftUI_ExaminationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_ExaminationApp.swift
+//  SwiftUI-Examination
+//
+//  Created by Sifatul on 27/6/26.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_ExaminationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ UIKit-Examination/UIKit-Examination/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIKit-Examination
+//
+//  Created by Sifatul on 27/6/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
