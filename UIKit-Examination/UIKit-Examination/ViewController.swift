aba4a24e88962afbada1f7db632f894a5b8123fe
Sifatul Islam <sifat.tn99456@gmail.com>
2026-06-30 12:40:31 +0600

    Present FrameVsBound

--- UIKit-Examination/UIKit-Examination/ViewController.swift
+++ UIKit-Examination/UIKit-Examination/ViewController.swift
@@ -9,6 +9,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func openFrameVsButtonPage(_ sender: UIButton) {
+        let storyboard = UIStoryboard(name: "FrameVsBound",
+                                      bundle: nil)
+        let frameVsBoundPage = storyboard.instantiateViewController(withIdentifier: "FrameVsBound")
+        frameVsBoundPage.modalPresentationStyle = .formSheet
+        self.present(frameVsBoundPage, animated: true)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
